695d6bc83bf3c4a71bbda5785886f0d3b2dd82ec
Marmik Patel <marmikp19@gmail.com>
2024-06-30 22:27:36 -0700

    update credentials

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,24 +9,14 @@ import {
 
 console.log(process.env.FIREBASE_STORAGE_BUCKET)
 
-// const firebaseConfig = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.FIREBASE_APP_ID,
-//     measurementId: process.env.FIREBASE_MEASUREMENT_ID
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBIXBzqspniH4x2Pxxt9k1TsGfPyhtYYZ0",
-    authDomain: "agent-af6de.firebaseapp.com",
-    projectId: "agent-af6de",
-    storageBucket: "agent-af6de.appspot.com",
-    messagingSenderId: "517194535593",
-    appId: "1:517194535593:web:167c07c2d121b9319a4fb8",
-    measurementId: "G-EKZEQSJV04"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
