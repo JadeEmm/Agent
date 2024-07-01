eba3374345c862e2100919775457db92660b6547
Marmik Patel <marmikp19@gmail.com>
2024-07-01 01:23:47 -0700

    modify page for seeker profile

--- app/seeker-profile/page.tsx
+++ app/seeker-profile/page.tsx
@@ -16,30 +16,30 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '../api/auth/[...nextauth]/route'
 import SingleListing from './_component/single-listing'
 import { redirect, useRouter } from 'next/navigation'
+import { SeekerProfileModel } from '@/schemas/seekerprofile'
 
-async function MyListingsPage() {
+async function SeekerProfilePage() {
     const session = await getServerSession(authOptions)
 
     if (!session) {
         redirect('/api/auth/signin')
     }
 
-    const myListings = await ItemModel.find({
+    const seekerProfile = await SeekerProfileModel.find({
         hostid: session?.user.id
     })
-
 
   return (
     <>
  
-        <h1 className='text-2xl sm:text-4xl py-8 font-bold'>Your Agent Profile</h1>
+        <h1 className='text-2xl sm:text-4xl py-8 font-bold'>Your Seeker Profile</h1>
 
         {/* dialog for adding items */}
         {
-            myListings.length == 0 ? 
+            seekerProfile.length == 0 ? 
             <Dialog>
                 <DialogTrigger asChild>
-                    <Button variant="outline"><Plus className='mr-4 h-4 w-4' />Create Agent Profile</Button>
+                    <Button variant="outline"><Plus className='mr-4 h-4 w-4' />Create Seeker Profile</Button>
                 </DialogTrigger>
                 <DialogContent>
                     <ListYourItemComponent />
@@ -51,8 +51,8 @@ async function MyListingsPage() {
 
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-4 py-8">
             {
-                myListings.length > 0 ? 
-                <SingleListing listings={myListings} />
+                seekerProfile.length > 0 ? 
+                <SingleListing listings={seekerProfile} />
                 :   
                 <p className='text-xl font-light p-4'>No Agent Profile</p>
 
@@ -62,4 +62,4 @@ async function MyListingsPage() {
   )
 }
 
-export default MyListingsPage
+export default SeekerProfilePage
