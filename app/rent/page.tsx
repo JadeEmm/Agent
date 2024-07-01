939ba06423ce0e370fc0fb4f15f6c2c7f1a2d542
Shaishav Shah <shaishav.shah04@gmail.com>
2024-07-01 11:27:47 -0600

    added view all agents on /rent page

--- app/rent/page.tsx
+++ app/rent/page.tsx
@@ -16,71 +16,10 @@ async function RentCategoryPage({
     const session = await getServerSession(authOptions)
     await connectToDB()
 
-    const items = await ItemModel.aggregate([
-        {
-            $match: {
-                category: params.category
-            }
-        },
-        {
-            $lookup: {
-                from: 'bookings',
-                let: { itemId: '$_id' },
-                pipeline: [
-                    { $match: { $expr: { $and: [{ $eq: ['$itemid', '$$itemId'] }] } } },
-                    {
-                        $group: {
-                            _id: null,
-                            numberOfBookings: {
-                                $sum: {
-                                    $cond: [{ $eq: ['$status', 'RETURNED'] }, 1, 0]
-                                }
-                            }
-                        }
-                    },
-                ],
-                as: 'bookings'
-            },
-        },
-        {
-            $lookup: {
-                from: 'users',
-                localField: 'hostid',
-                foreignField: '_id',
-                as: 'host'
-            },
-        },
-        {
-            $unwind: { path: '$bookings', preserveNullAndEmptyArrays: true }
-        },
-        {
-            $unwind: '$host'
-        },
-        {
-            $match: { 'host._id': { $ne: new ObjectId(session?.user.id) } }
-        },
-        {
-            $group: {
-                _id: '$_id',
-                name: { $first: '$name' },
-                hostName: { $first: '$host.name' },
-                price: { $first: '$price' },
-                photos: { $first: '$photos' },
-                numberOfBookings: {
-                    $sum: '$bookings.numberOfBookings'
-                }
-            }
-        },
-        {
-            $project: {
-                name: 1,
-                hostName: '$host.name',
-                price: 1,
-                photos: 1,
-                numberOfBookings: 1
-            }
-        }
-    ])
+    // get all items
+    const items = await ItemModel.find({}).lean();
+
+    console.log(items)
 
 
     return (
@@ -93,7 +32,7 @@ async function RentCategoryPage({
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 sm:gap-6 py-4">
                 {
                     items.map(item => (
-                        <Card key={item._id} className='flex flex-col items-center hover:shadow-md'>
+                        <Card key ={item._id} className='flex flex-col items-center hover:shadow-md'>
                             <CardContent className='px-0'>
                                 <Image
                                     className='p-0 max-h-60 sm:max-h-60' alt={item.name}
@@ -119,7 +58,7 @@ async function RentCategoryPage({
 
                                     <hr className='pt-1 pb-1' />
                                     <div className="flex justify-between leading-6">
-                                        <div className="text-slate-500">Past bookings</div>
+                                        <div className="text-slate-500">Rating</div>
                                         <p>{item.numberOfBookings}</p>
                                     </div>
                                 </div>
@@ -129,11 +68,11 @@ async function RentCategoryPage({
                                     session ?
                                         <Link
                                             className='font-bold text-xl text-primary'
-                                            href={`item/${item._id}`}>Rent now</Link>
+                                            href={`item/${item._id}`}>Contact Now</Link>
                                         :
                                         <Link
                                             className='text-blue-500'
-                                            href='/auth/sign-in'>Login to rent</Link>
+                                            href='/auth/sign-in'>Login to contact</Link>
                                 }
                             </CardFooter>
                         </Card>
--- components/browse-items.tsx
+++ components/browse-items.tsx
@@ -32,18 +32,18 @@ function BrowseItems() {
             </div> */}
             <div className="flex flex-col sm:flex-row gap-4">
 
-                            <Card className='flex flex-col items-center hover:shadow-md'>
-                                <CardHeader>
-                                    <CardTitle>Ayushe</CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <Image alt={`ayushe`} src={`/rent-camera-gear.jpg`}
-                                        width={200} height={100} />
-                                </CardContent>
-                                <CardFooter>
-                                    <Link href={`/rent/$camera-gear`}>Browse &rarr;</Link>
-                                </CardFooter>
-                            </Card>
+                <Card className='flex flex-col items-center hover:shadow-md'>
+                    <CardHeader>
+                        <CardTitle>SWE</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Image alt={`ayushe`} src={`/rent-camera-gear.jpg`}
+                            width={200} height={100} />
+                    </CardContent>
+                    <CardFooter>
+                        <Link href={`/rent`}>Browse &rarr;</Link>
+                    </CardFooter>
+                </Card>
 
             </div>
 
