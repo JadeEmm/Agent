5919440abcbb73c0a8785d3ce2587ff0f0b5dd9a
Shaishav Shah <shaishav.shah04@gmail.com>
2024-07-04 23:00:51 -0600

    Agent card on jobs page.

--- app/dashboard/seeker/jobs/page.tsx
+++ app/dashboard/seeker/jobs/page.tsx
@@ -8,12 +8,15 @@ import {
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { Plus } from 'lucide-react'
-import { ItemModel } from '@/schemas/item'
+import { JobApplicationModel } from '@/schemas/jobApplication'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../../../api/auth/[...nextauth]/route'
 import { redirect, useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+
 
 async function MainDashboardJobs() {
     const session = await getServerSession(authOptions)
@@ -22,15 +25,61 @@ async function MainDashboardJobs() {
         redirect('/api/auth/signin')
     }
 
-    const myListings = await ItemModel.find({
-        hostid: session?.user.id
-    })
+    const myAppliedJobs = await JobApplicationModel.find({
+        seekerid: session?.user.id
+    }) 
 
+    // TODO: retrive actual agent once the model is updated.
+    const assignedAgent = {
+        id: '123',
+        name: 'John Doe',
+        email: '',
+        area: ["SWE", "Data Science"],
+        image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
+    } 
 
   return (
-    <>
-        <h1 className='text-2xl sm:text-4xl py-8 font-bold'>Seeker Jobs Edit</h1>  
-    </>
+    <div className="flex flex-col">
+      <div className="border border-red w-full h-64 flex flex-row">
+        <div className='border border-gray w-1/2 p-4'>
+          <h3><strong>Your Agent: </strong></h3>
+
+              <div className="relative flex w-full max-w-96 flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md h-40 m-4">
+                <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+                  <img
+                    src={assignedAgent.image}
+                    alt="image"
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+                <div className="p-2 w-3/5">
+                  <h6 className="ml-1 mb-1 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-slate-800 antialiased">
+                    {assignedAgent.name}
+                  </h6>
+                  <p className="mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+                    {assignedAgent.area.map((area, index) => (
+                    <span className="text-sm font-medium bg-slate-100 py-1 px-2 rounded text-slate-500 align-middle mx-1" key={index}>{area}</span>
+                    ))}
+                  </p>
+                  <Link
+                    className={cn(buttonVariants({variant: 'ghost'}),"w-full mt-2")}
+                    href={`/agent/${assignedAgent.id}`}
+                  >
+                    View More
+                  </Link>
+                </div>
+              </div>
+          
+        </div>
+        <div className='border border-gray w-1/2 p-4'>
+        <h3><strong>Your Preferences: </strong></h3>
+        </div>
+      </div>
+      <div className="border flex-1 border-red w-full h-80 max-h-full p-6">
+        <h3><strong>Applications: </strong></h3>
+      </div>
+        {/* <h1 className='text-2xl sm:text-4xl py-8 font-bold'>Seeker Jobs Edit</h1>   */}
+    </div>
   )
 }
 
