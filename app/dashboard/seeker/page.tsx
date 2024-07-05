6024261dd2eeabcc2a5106148f1b4939c1f95995
Marmik Patel <marmikp19@gmail.com>
2024-07-05 03:24:40 -0700

    create basic dashboard layout

--- app/dashboard/seeker/page.tsx
+++ app/dashboard/seeker/page.tsx
@@ -1,19 +1,11 @@
-
-
 import React from 'react'
-import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button'
-import { Plus } from 'lucide-react'
-import { ItemModel } from '@/schemas/item'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../../api/auth/[...nextauth]/route'
 import { redirect, useRouter } from 'next/navigation'
+import Image from 'next/image';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils'
 
 async function MainDashboard() {
     const session = await getServerSession(authOptions)
@@ -22,15 +14,53 @@ async function MainDashboard() {
         redirect('/api/auth/signin')
     }
 
-    const myListings = await ItemModel.find({
-        hostid: session?.user.id
-    })
-
+    // temporary consts until fetching is possible
+    const agent = {name: "Shaishav Shah" };
+    const seeker = {numApps: 20, numCredits: 30, photos: ["./agent-may.jpg"]};
+    const increaseCredit = "/";
 
   return (
-    <>
-        <h1 className='text-2xl sm:text-4xl py-8 font-bold'>Seeker Dashboard</h1>  
-    </>
+    // <>
+    //     <h1 className='text-2xl sm:text-4xl py-8 font-bold'>Seeker Dashboard</h1>  
+    // </>
+  
+ 
+          
+
+          <div className="grid gap-x-4 gap-y-4 grid-cols-2 mt-20">
+            <div className="flex items-center justify-center mr-0">
+              <div className="flex flex-col items-center bg-lime-200 rounded-lg p-6">
+                <h3 className="text-xl mb-4">Applications Completed</h3>
+                <p className="text-xl font-bold mb-4">{seeker.numApps}</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="flex bg-purple-100 rounded-lg p-6 max-w-md">
+                <div className="flex flex-col pr-5">
+                  <h3 className="text-xl mb-2">Your Agent</h3>
+                  <p className="text-xl font-bold mb-2">{agent.name}</p>
+                </div>
+                <div className="flex flex-col items-center mr-4">
+                  <Image alt="agent-may" src="/agent-may.jpg" width={50} height={80} className="mb-4 rounded-lg" />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <div className="flex bg-orange-100 rounded-lg p-6 max-w-md">
+                <div className="flex flex-col pr-5">
+                  <h3 className="text-xl mb-4">Credit:</h3>
+                  <p className="text-xl font-bold mb-4">${seeker.numCredits}</p>
+                </div>
+                <div className="flex flex-col items-center mr-4">
+                  <Link href={increaseCredit} className={cn(buttonVariants({variant: 'secondary'}),"")} >
+                      Increase credit
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        
   )
 }
 
