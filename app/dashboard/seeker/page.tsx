2b98851cfeabc20d2c2b5d49fd1b52697eb2372f
Marmik Patel <marmikp19@gmail.com>
2024-07-06 13:00:17 -0700

    link agent to dashboard

--- app/dashboard/seeker/page.tsx
+++ app/dashboard/seeker/page.tsx
@@ -7,6 +7,8 @@ import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils'
 import { SeekerProfileModel } from '@/schemas/seekerprofile';
+import { ItemModel } from '@/schemas/item';
+import { ImageIcon } from 'lucide-react';
 
 async function MainDashboard() {
     const session = await getServerSession(authOptions)
@@ -15,15 +17,18 @@ async function MainDashboard() {
         redirect('/api/auth/signin')
     }
 
-    let seekerProfiles = await SeekerProfileModel.find({
+    const seekerProfiles = await SeekerProfileModel.find({
       hostid: session?.user.id
     });
 
-    const linkToJobs = "/dashboard/seeker/jobs"
+    // active agent
+    const agents = seekerProfiles[0].agentid ? await ItemModel.find({
+      hostid: seekerProfiles[0].agentid
+    }) : null;
 
-    // temporary consts until fetching is possible
-    const agent = {name: "Shaishav Shah" };
-    const increaseCredit = "/";
+    const linkToJobs = "/dashboard/seeker/jobs"
+    const connectAgent = "/" // update when page to connect with an agent is live
+    const increaseCredit = "/"; // update when stripe page is live
 
   return (
       <div className="max-w-xl mx-auto text-center">
@@ -31,25 +36,39 @@ async function MainDashboard() {
           <div className="grid gap-x-4 gap-y-4 grid-cols-2 mt-20">
             <div className="flex items-center justify-center mr-0">
               <Link href={linkToJobs}>
-                <div className="flex flex-col items-center bg-lime-200 rounded-lg p-6">
+                <div className="flex flex-col items-center bg-lime-200 rounded-lg p-6 w-50 h-36">
                   <h3 className="text-xl mb-4">Applications Completed</h3>
                   <p className="text-xl font-bold mb-4">{seekerProfiles[0].numApps ? seekerProfiles[0].numApps : 0}</p>
                 </div>
               </Link>
             </div>
             <div className="flex items-center justify-center">
-              <div className="flex bg-purple-100 rounded-lg p-6 max-w-md">
+              <div className="flex bg-purple-100 rounded-lg p-6 w-50 h-36">
                 <div className="flex flex-col pr-5">
-                  <h3 className="text-xl mb-2">Your Agent</h3>
-                  <p className="text-xl font-bold mb-2">{agent.name}</p>
+                {agents ? <h3 className="text-xl mb-2">Your Agent</h3> : <h3 className="text-xl mb-2">No Agent Yet</h3>}
+                  {agents ? 
+                    <p className="text-xl font-bold mb-2">{agents[0].name}</p> : 
+                    <Link href={connectAgent} className={cn(buttonVariants({variant: 'secondary'}),"")}>
+                        Connect with an agent
+                    </Link>}
                 </div>
                 <div className="flex flex-col items-center mr-4">
-                  <Image alt="agent-may" src="/agent-may.jpg" width={50} height={80} className="mb-4 rounded-lg" />
+                  {agents ?        
+                    (
+                      agents[0].photos.length > 0 ?
+                        <Image alt={agents[0].name} src={`${agents[0].photos.at(0)}`} 
+                        width={50} height={80} className="mb-4 rounded-lg" /> 
+                        :
+                        <ImageIcon width={100} height={100} className='text-slate-200' />
+                    )
+                    :
+                    <></>
+                    }             
                 </div>
               </div>
             </div>
             <div className="flex items-center justify-center">
-              <div className="flex bg-orange-100 rounded-lg p-6 max-w-md">
+              <div className="flex bg-orange-100 rounded-lg p-6 w-60 h-36">
                 <div className="flex flex-col pr-5">
                   <h3 className="text-xl mb-4">Credit:</h3>
                   <p className="text-xl font-bold mb-4">${seekerProfiles[0].numCredits ? seekerProfiles[0].numCredits : 0}</p>
