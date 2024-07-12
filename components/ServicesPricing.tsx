a4aac826753cd3aa2b883bf85a8eb1447ae748e3
JadeEmm <144599814+JadeEmm@users.noreply.github.com>
2024-07-12 17:29:51 +0100

    Update Pricing Page

--- components/ServicesPricing.tsx
+++ components/ServicesPricing.tsx
@@ -19,32 +19,49 @@ const ServicesPricing: React.FC = () => {
           <div className="mb-16">
             <h2 className="text-4xl sm:text-5xl font-bold theme-text mb-4">Our Pricing Plans</h2>
             <p className="text-xl mb-8">
-              Choose the plan that best fits your needs and start leveraging our platform for maximum benefit.
+              Choose the plan that best fits your needs that your agent will do for you .
             </p>
           </div>
 
           <div className="grid grid-cols-2 gap-8">
             <div className="col-span-1">
               <div className="card p-6 rounded-lg shadow-lg">
-                <h3 className="text-3xl font-bold mb-4">Basic Plan</h3>
-                <p className="text-2xl mb-4">$49/month</p>
-                <p className="text-left mb-4">Access to basic tools</p>
-                <p className="text-left mb-4">10 job applications per month</p>
-                <p className="text-left mb-4">Email support</p>
-                <p className="text-left mb-4">Basic analytics</p>
-                <button className="bg-blue-800 text-white py-3 px-6 rounded-md font-bold text-xl hover:bg-blue-600 transition duration-300">Choose Basic</button>
+                <h3 className="text-3xl font-bold mb-4">Tier 1</h3>
+                <p className="text-2xl mb-4">1 Credit per Application</p>
+                <p className="text-left mb-4">Chat with your agent</p>
+                <p className="text-left mb-4">Finds your job or internship postings</p>
+                <p className="text-left mb-4">Submits your applications</p>
+                <p className="text-left mb-4">Application tracking</p>
+                <button className="bg-blue-800 text-white py-3 px-6 rounded-md font-bold text-xl hover:bg-blue-600 transition duration-300">Choose Tier 1</button>
               </div>
             </div>
             <div className="col-span-1">
               <div className="card p-6 rounded-lg shadow-lg">
-                <h3 className="text-3xl font-bold mb-4">Premium Plan</h3>
-                <p className="text-2xl mb-4">$99/month</p>
-                <p className="text-left mb-4">Access to all tools</p>
-                <p className="text-left mb-4">Unlimited job applications</p>
-                <p className="text-left mb-4">Priority email and chat support</p>
-                <p className="text-left mb-4">Advanced analytics</p>
-                <p className="text-left mb-4">Personalized coaching</p>
-                <button className="bg-blue-800 text-white py-3 px-6 rounded-md font-bold text-xl hover:bg-blue-600 transition duration-300">Choose Premium</button>
+                <h3 className="text-3xl font-bold mb-4">Tier 2</h3>
+                <p className="text-2xl mb-4">2 Credits per Application</p>
+                <p className="text-left mb-4">Chat with your agent</p>
+                <p className="text-left mb-4">Finds your job or internship postings</p>
+                <p className="text-left mb-4">Tailors and customizes your resume</p>
+                <p className="text-left mb-4">Submits your applications</p>
+                <p className="text-left mb-4">Application Tracking</p>
+                <button className="bg-blue-800 text-white py-3 px-6 rounded-md font-bold text-xl hover:bg-blue-600 transition duration-300">Choose Tier 2</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8 mb-16">
+            <h2 className="text-4xl sm:text-3xl font-bold theme-text mb-4">Get more interviews with early applications</h2>
+            <h3 className="text-xl mb-8"> Studies show the sooner you apply to a job after it's been posted, the better your chances of landing an interview </h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <div className="card p-6 rounded-lg shadow-lg">
+                <h3 className="text-3xl font-bold mb-4">Fast Track Add-On</h3>
+                <p className="text-2xl mb-4">10 Credits per Order</p>
+                <p className="text-xl mb-4">Add to any plan for:</p>
+                <p className="text-left mb-4">Faster application processing.</p>
+                <p className="text-left mb-4">Rapid application submission</p>
+                <p className="text-left mb-4">Priority handling</p>
+
+                <button className="bg-blue-800 text-white py-3 px-6 rounded-md font-bold text-xl hover:bg-blue-600 transition duration-300">Add Fast Track</button>
               </div>
             </div>
           </div>
@@ -54,15 +71,15 @@ const ServicesPricing: React.FC = () => {
             <div className="grid grid-cols-3 gap-8">
               <div className="step-card p-6 rounded-lg shadow-lg">
                 <h3 className="text-3xl font-bold mb-4">Step 1</h3>
-                <p className="text-xl mb-4">Sign up for an account</p>
+                <p className="text-xl mb-4">Sign up for an account and set your profile and preferences</p>
               </div>
               <div className="step-card p-6 rounded-lg shadow-lg">
                 <h3 className="text-3xl font-bold mb-4">Step 2</h3>
-                <p className="text-xl mb-4">Choose your preferred plan</p>
+                <p className="text-xl mb-4">Add to your balance to gain credits for applications</p>
               </div>
               <div className="step-card p-6 rounded-lg shadow-lg">
                 <h3 className="text-3xl font-bold mb-4">Step 3</h3>
-                <p className="text-xl mb-4">Start using our platform</p>
+                <p className="text-xl mb-4">Hire an agent and choose your plan</p>
               </div>
             </div>
           </div>
