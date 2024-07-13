682b6fcc984d6ff2a9d26414b5b8f48379a7f1d2
JadeEmm <144599814+JadeEmm@users.noreply.github.com>
2024-07-13 11:00:54 +0100

    Update contact us

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -46,27 +46,30 @@ const ContactUs: React.FC = () => {
           {/* Right contact page */}
           <div className="direct-contact-container">
             <ul className="contact-list">
-              <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">City, State</span></i></li>
-              <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel:1-212-555-5555" title="Give me a call">(212) 555-2368</a></span></i></li>
-              <li className="list-item"><i className="fa fa-envelope fa-2x"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">hitmeup@gmail.com</a></span></i></li>
+              <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">San Francisco, California</span></i></li>
+              <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel:XX" title="Give me a call">(XXX) XXX-XXXX</a></span></i></li>
+              <li className="list-item"><i className="fa fa-envelope fa-2x"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">helloagent@mail.com</a></span></i></li>
             </ul>
 
-            <hr />
-            <ul className="social-media-list">
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-github" aria-hidden="true"></i></a>
-              </li>
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-codepen" aria-hidden="true"></i></a>
-              </li>
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-twitter" aria-hidden="true"></i></a>
-              </li>
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-instagram" aria-hidden="true"></i></a>
-              </li>
-            </ul>
-            <hr />
+{/* 
+<hr />
+<ul className="social-media-list">
+  <li><a href="#" target="_blank" className="contact-icon">
+    <i className="fa fa-github" aria-hidden="true"></i></a>
+  </li>
+  <li><a href="#" target="_blank" className="contact-icon">
+    <i className="fa fa-codepen" aria-hidden="true"></i></a>
+  </li>
+  <li><a href="#" target="_blank" className="contact-icon">
+    <i className="fa fa-twitter" aria-hidden="true"></i></a>
+  </li>
+  <li><a href="#" target="_blank" className="contact-icon">
+    <i className="fa fa-instagram" aria-hidden="true"></i></a>
+  </li>
+</ul>
+<hr />
+*/}
+
           </div>
         </div>
       </section>
