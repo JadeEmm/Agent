c73ac991bf1c787445e0595c380b585fd788b2f2
Shaishav Shah <shaishav.shah04@gmail.com>
2024-07-16 15:48:26 -0600

    chore: Update AgentProfileForm description placeholder text

--- components/profileFormAgent.tsx
+++ components/profileFormAgent.tsx
@@ -66,8 +66,8 @@ export function AgentProfileForm(
                   <textarea
                     id="description"
                     {...register("description", { required: "Description is required" })}
-                    placeholder='Tell us about yourself!'
-                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                    placeholder='This is a description of your services, your experience, etc. Be as detailed as possible.'
+                    className="block w-full h-60 rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                   />
                   {errors.description && <span className="text-red-600 text-sm">Description is required</span>}
                 </div>
