6bcac4fa405315c96126bf671c7ad6232beee65b
Marmik Patel <marmikp19@gmail.com>
2024-07-18 11:13:22 -0700

    disable form if no credits

--- components/orderForm.tsx
+++ components/orderForm.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import React, { useState } from 'react'
+import React from 'react'
 import { useForm,  } from 'react-hook-form';
-import { SeekerProfile, Tier } from '@/types';
+import { Tier } from '@/types';
 
 export function OrderForm(
   { user_id, existingProfile }: 
@@ -34,6 +34,7 @@ export function OrderForm(
                             type="radio"
                             value={Tier.One}
                             {...register("pricingTier", { required: true })}
+                            disabled={!existingProfile || existingProfile?.numCredits == 0}
                             className="mr-2"
                             />
                             <label htmlFor="tierI" className="text-sm font-medium leading-6 text-gray-900">Tier I - $1.49/application</label>
@@ -50,6 +51,7 @@ export function OrderForm(
                             type="radio"
                             value={Tier.Two}
                             {...register("pricingTier", { required: true })}
+                            disabled={!existingProfile || existingProfile?.numCredits == 0}
                             className="mr-2"
                             />
                             <label htmlFor="tierII" className="text-sm font-medium leading-6 text-gray-900">Tier II - $2.99/application</label>
@@ -76,6 +78,7 @@ export function OrderForm(
                                 id="numApps"
                                 {...register("numApps", { required: true })}
                                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                                disabled={!existingProfile || existingProfile?.numCredits == 0}
                                 />
                                 {errors.numApps && <span className="text-red-600 text-sm">Number of applications is required</span>}
                             </div>
@@ -84,8 +87,8 @@ export function OrderForm(
             </div>
 
             <div className="mt-6 flex items-center justify-start gap-x-6">
-                <button type="button" className="text-sm font-semibold leading-6 text-gray-900" onClick={() => reset()}>Cancel</button>
-                <button type="submit" className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 cursor-pointer focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+                <button type="button" disabled={!existingProfile || existingProfile?.numCredits == 0} className="text-sm font-semibold leading-6 text-gray-900" onClick={() => reset()}>Cancel</button>
+                <button type="submit" disabled={!existingProfile || existingProfile?.numCredits == 0} className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 cursor-pointer focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
                     Create Order
                 </button>
             </div>
